e40dda654c98765a4df6834f45df0890bbf1763f
MD Ashikur Rahman <71284265+md-ashikur@users.noreply.github.com>
2023-06-01 12:53:48 +0600

    updated

--- src/partials/accueil/AccueilBody.jsx
+++ src/partials/accueil/AccueilBody.jsx
@@ -8,25 +8,28 @@ import welcomeBg from '../../img/intro welcome page background.png'
 const AccueilBody = () => {
     return (
         <div className=" ">
-        <div className=" relative -mt-20 overflow-hidden z-10">
+        <div className=" relative  overflow-hidden z-20">
           <div className="h-screen w-full -mt-7">
             <img
               src={welcomeBg}
               alt=""
               draggable={false}
-              className="object-cover lg:object-none h-full lg:h-auto lg:w-full"
+              className="object-cover lg:object-none h-full lg:h-screen lg:w-full"
             />
           </div>
   
-          <div className="absolute top-20 ">
+          <div className="absolute top-10 ">
             <Wheader />
           </div>
         
         </div>
-      <section className="absolute top-20">  <WsecOne /></section>
+      <section className=" top-20 z-20">  
+      <WsecOne />
+      </section>
         <section className="relative z-20">
-          {/* <WSecTwo /> */}
+          <WSecTwo />
         </section>
+
 
         <WSecThree />
       </div>
