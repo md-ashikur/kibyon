1d2aebe022e605145a3b1aeac0e7a3e8c943ae13
MD Ashikur Rahman <71284265+md-ashikur@users.noreply.github.com>
2023-06-14 14:59:32 +0600

    slider color fixed

--- src/partials/offerPage/OfferHeader.jsx
+++ src/partials/offerPage/OfferHeader.jsx
@@ -61,7 +61,7 @@ const OfferHeader = () => {
         <div className="rounded-lg  bg-gradient-to-t from-[#080E1A]  to-[#1A3D5F]  ">
               <h3 className="text-lg font-bold pt-3">Étape 1 </h3>
               <img src={img1} alt="" className="!w-[300px] !h-[300px]" />
-              <p className="text-xs bg-[#080E1A] text-justify  rounded-b-lg p-3">
+              <p className="text-xs font-bold bg-[#080E1A] text-justify  rounded-b-lg p-3">
               1 échange téléphonique pour bien comprendre votre projet 
               </p>
             </div>
@@ -74,7 +74,7 @@ const OfferHeader = () => {
              <div className="h-[280px] overflow-hidden">
              <img src={img2} alt="" className="" />
              </div>
-              <p className="text-xs bg-[#9F3936] text-justify  rounded-b-lg p-3">
+              <p className="text-xs font-bold bg-[#9F3936] text-justify  rounded-b-lg p-3">
               Collecte des éléments disponibles du projet et collaboration pour obtenir les éléments manquants 
               </p>
             </div>
@@ -83,7 +83,7 @@ const OfferHeader = () => {
         <div className="rounded-lg bg-gradient-to-t from-[#A49580] to-[#EBECEC]">
               <h3 className="text-lg font-bold pt-3">Étape 3 </h3>
               <img src={img3} alt="" className="" />
-              <p className="text-xs bg-[#A49580] text-justify  rounded-b-lg p-3">
+              <p className="text-xs font-bold bg-[#A49580] text-justify  rounded-b-lg p-3">
               1 proposition commerciale adaptée à votre besoin  
               </p>
             </div>
@@ -92,7 +92,7 @@ const OfferHeader = () => {
         <div className="rounded-lg bg-gradient-to-t from-[#434551] to-[#E9D0BF]">
               <h3 className="text-lg font-bold pt-3">Étape 4 </h3>
               <img src={img4} alt="" className="" />
-              <p className="text-xs bg-[#161A26] text-justify  rounded-b-lg p-3">
+              <p className="text-xs font-bold bg-[#161A26] text-justify  rounded-b-lg p-3">
               Entretiens de travail de plusieurs heures pour structurer le projet et définir les lignes directrices 
               </p>
             </div>
@@ -101,7 +101,7 @@ const OfferHeader = () => {
         <div className="rounded-lg bg-gradient-to-t to-[#076167] from-[#000000] to-90%">
               <h3 className="text-lg font-bold pt-3">Étape 5 </h3>
               <img src={img5} alt="" className="" />
-              <p className="text-xs bg-[#01191a] text-justify  rounded-b-lg p-3">
+              <p className="text-xs font-bold bg-[#01191a] text-justify  rounded-b-lg p-3">
               Plusieurs échanges complémentaires au fil de la mission pour préciser le projet et réfléchir ensemble aux meilleurs choix pour vous 
               </p>
             </div>
@@ -110,7 +110,7 @@ const OfferHeader = () => {
         <div className="rounded-lg bg-gradient-to-t from-[#678CA7] to-[#B4CEDB] to-90%">
               <h3 className="text-lg font-bold pt-3">Étape 6 </h3>
               <img src={img6} alt="" className="" />
-              <p className="text-xs bg-[#678CA7] text-justify  rounded-b-lg p-3">
+              <p className="text-xs font-bold bg-[#678CA7] text-justify  rounded-b-lg p-3">
               Livrable du projet en pdf et Powerpoint avec accès au fichier source (pour la partie financière notamment) 
               </p>
             </div>
@@ -121,7 +121,7 @@ const OfferHeader = () => {
               <div className="h-[280px] overflow-hidden">
              <img src={img7} alt="" className="" />
              </div>
-              <p className="text-xs bg-[#27445A] text-justify  rounded-b-lg p-3">
+              <p className="text-xs font-bold bg-[#27445A] text-justify  rounded-b-lg p-3">
               Présentation du projet et collaboration avec vous pour assurer une prise en main complète, favorisant ainsi votre totale indépendance et autonomie
               </p>
             </div>
@@ -132,7 +132,7 @@ const OfferHeader = () => {
         <div className="rounded-lg bg-gradient-to-t from-[#678CA7] to-[#B4CEDB] to-90%">
               <h3 className="text-lg font-bold pt-3">Étape 6 </h3>
               <img src={img6} alt="" className="" />
-              <p className="text-xs bg-[#678CA7] text-justify  rounded-b-lg p-3">
+              <p className="text-xs font-bold bg-[#678CA7] text-justify  rounded-b-lg p-3">
               Livrable du projet en pdf et Powerpoint avec accès au fichier source (pour la partie financière notamment) 
               </p>
             </div>
@@ -143,7 +143,7 @@ const OfferHeader = () => {
               <div className="h-[280px] overflow-hidden">
              <img src={img7} alt="" className="" />
              </div>
-              <p className="text-xs bg-[#27445A] text-justify  rounded-b-lg p-3">
+              <p className="text-xs font-bold bg-[#27445A] text-justify  rounded-b-lg p-3">
               Présentation du projet et collaboration avec vous pour assurer une prise en main complète, favorisant ainsi votre totale indépendance et autonomie
               </p>
             </div>
@@ -164,7 +164,7 @@ const OfferHeader = () => {
 
 
       <div className="flex justify-center py-10">
-      <p className="text-center lg:w-[60%] w-[80%] text-white text-sm">
+      <p className="text-justify w-[80%] text-white ">
             Voici un aperçu de notre processus de missions. Cependant, afin de
             vous proposer une offre sur mesure, nous adaptons nos méthodes à
             votre projet, à votre façon de travailler et aux aléas éventuels.
