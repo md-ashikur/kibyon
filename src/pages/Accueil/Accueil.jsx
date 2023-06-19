f29d67113a2f5a105a2b694776fb73a5b4822edd
MD Ashikur Rahman <71284265+md-ashikur@users.noreply.github.com>
2023-06-20 01:42:53 +0600

    home page updating

--- src/pages/Accueil/Accueil.jsx
+++ src/pages/Accueil/Accueil.jsx
@@ -6,7 +6,7 @@ import AccueilBody from "../../partials/accueil/AccueilBody";
 
 const Accueil = () => {
   return (
-    <div className="flex overflow-hidden">
+    <div className="flex h-screen overflow-hidden">
     
     <div className="relative flex flex-col flex-1 overflow-y-auto overflow-x-hidden">
       <Header  />
